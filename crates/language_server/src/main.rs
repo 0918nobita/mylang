09d13828f03401386195f810c0adb151c7876df2
0918nobita <nobita.0918@gmail.com>
2022-02-04 16:23:21 +0900

    :construction: 言語サーバで initialize メッセージを受信・パースするようにした

--- crates/language_server/src/main.rs
+++ crates/language_server/src/main.rs
@@ -1,1 +1,50 @@
-fn main() {}
+use std::io::{self, BufRead, Read};
+
+use anyhow::Context;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+
+/// JSON-RPC メッセージ
+#[derive(Debug, Serialize, Deserialize)]
+struct Message {
+    #[serde(rename = "jsonrpc")]
+    version: String,
+
+    id: usize,
+
+    method: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+
+    let mut buf = String::new();
+
+    let re = Regex::new("Content-Length: (\\d+)")?;
+
+    loop {
+        let num_bytes = stdin.read_line(&mut buf)?;
+        buf = buf.trim().to_owned();
+        stdin.consume(num_bytes);
+
+        if let Some(caps) = re.captures(&buf) {
+            let len = caps
+                .get(1)
+                .context("Failed to parse header")?
+                .as_str()
+                .parse::<usize>()?;
+            eprintln!("Length: {}", len);
+
+            let mut content_buf = vec![0u8; len];
+            stdin.read_exact(&mut content_buf)?;
+            stdin.consume(len);
+
+            let content = String::from_utf8(content_buf)?;
+            eprintln!("Content: {}", content);
+
+            let msg: Message = serde_json::from_str(&content)?;
+            eprintln!("Deserialized: {:?}", msg);
+        }
+    }
+}

c41d1d129be6a9523d3166c4f09e3a347fe9cec9
0918nobita <0918nobita@users.noreply.github.com>
2022-02-06 15:09:08 +0000

    deploy: 9257fc958b5982ec7a5dc7f81c1a166d21503c66

--- lsp_server/responder/sidebar-items.js
+++ lsp_server/responder/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["lex",""],["lex_and_report_errs",""],["lex_err_to_diagnostic",""],["locatable_to_json_range",""],["text_document_uri",""]],"struct":[["Responder",""]]});
+initSidebarItems({"fn":[["analyze_src_and_report_errs",""],["lex",""],["lex_err_to_diagnostic",""],["locatable_to_json_range",""],["parse_err_to_diagnostic",""],["text_document_uri",""]],"struct":[["Responder",""]]});

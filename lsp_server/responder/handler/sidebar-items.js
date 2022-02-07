d52638799235ff8dd6f0c418faa8542349c08f33
0918nobita <0918nobita@users.noreply.github.com>
2022-02-07 14:28:56 +0000

    deploy: 998a1012c4f84d7b76d9d457bae61df207311fd1

--- lsp_server/responder/handler/sidebar-items.js
+++ lsp_server/responder/handler/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["handle",""],["handle_did_change_notification",""],["handle_did_close_notification",""],["handle_did_open_notification",""],["handle_initialize_request",""],["text_document_uri",""]]});
+initSidebarItems({"fn":[["handle",""],["handle_did_change_notification",""],["handle_did_close_notification",""],["handle_did_open_notification",""],["handle_initialize_request",""],["text_document_uri",""],["token_types",""]]});

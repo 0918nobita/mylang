ed32df3c1aec67e17a6a699c0cf0937a6ecda672
0918nobita <nobita.0918@gmail.com>
2022-02-07 15:49:39 +0900

    :sparkles: 構文エラーの発生位置を報告するようにした

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -10,72 +10,77 @@
 use ast::{Expr, Stmt};
 use itertools::{put_back, PutBack};
 use thiserror::Error;
-use token::{KeywordKind, Locatable, Range, Token};
+use token::{KeywordKind, Locatable, Pos, Range, Token};
 
 /// 構文解析中に発生するエラー
 #[allow(dead_code)]
 #[derive(Debug, Error)]
 pub enum ParseErr {
-    #[error("Unexpected token")]
-    UnexpectedToken,
+    #[error("({0}) Term expected, but not found")]
+    TermExpected(Pos),
 
-    #[error("Term expected, but not found")]
-    TermExpected,
-
-    #[error("Add operator expected, but not found")]
-    AddOpExpected,
-
-    #[error("Either print_int or print_str expected, but not found")]
-    KeywordExpected,
+    #[error("({0}) Either print_int or print_str expected, but not found")]
+    KeywordExpected(Range),
 }
 
 impl Locatable for ParseErr {
     fn locate(&self) -> Range {
-        // TODO: 正確な範囲を返す
-        Range::default()
+        match self {
+            ParseErr::TermExpected(pos) => pos.clone().into(),
+            ParseErr::KeywordExpected(range) => range.clone(),
+        }
     }
 }
 
-fn term(tokens: &mut impl Iterator<Item = Token>) -> Result<Expr, ParseErr> {
-    let tok = tokens.next().ok_or(ParseErr::TermExpected)?;
-
-    match tok {
-        Token::I32(range, n) => Ok(Expr::I32Lit(range, n)),
-        Token::Str(range, s) => Ok(Expr::StrLit(range, s)),
-        _ => Err(ParseErr::TermExpected),
+fn term(
+    tokens: &mut PutBack<impl Iterator<Item = Token>>,
+    pos: Pos,
+) -> Result<(Pos, Expr), ParseErr> {
+    if let Some(tok) = tokens.next() {
+        match tok {
+            Token::I32(range, n) => Ok((range.end.clone(), Expr::I32Lit(range, n))),
+            Token::Str(range, s) => Ok((range.end.clone(), Expr::StrLit(range, s))),
+            _ => Err(ParseErr::TermExpected(tok.locate().end)),
+        }
+    } else {
+        Err(ParseErr::TermExpected(pos))
     }
 }
 
-fn expr(tokens: &mut PutBack<impl Iterator<Item = Token>>) -> Result<Expr, ParseErr> {
-    let lhs = term(tokens)?;
+fn expr(
+    tokens: &mut PutBack<impl Iterator<Item = Token>>,
+    pos: Pos,
+) -> Result<(Pos, Expr), ParseErr> {
+    let (pos, lhs) = term(tokens, pos)?;
 
     match tokens.next() {
         Some(Token::AddOp(_)) => {
-            let rhs = expr(tokens)?;
+            let (pos, rhs) = expr(tokens, pos)?;
             Ok(match rhs {
-                Expr::Add(b, c) => Expr::Add(Box::new(Expr::Add(Box::new(lhs), b)), c),
-                _ => Expr::Add(Box::new(lhs), Box::new(rhs)),
+                Expr::Add(b, c) => (pos, Expr::Add(Box::new(Expr::Add(Box::new(lhs), b)), c)),
+                _ => (pos, Expr::Add(Box::new(lhs), Box::new(rhs))),
             })
         }
         Some(tok) => {
             tokens.put_back(tok);
-            Ok(lhs)
+            Ok((pos, lhs))
         }
-        None => Ok(lhs),
+        None => Ok((pos, lhs)),
     }
 }
 
-fn stmt(tokens: &mut PutBack<impl Iterator<Item = Token>>) -> Result<Stmt, ParseErr> {
+fn stmt(tokens: &mut PutBack<impl Iterator<Item = Token>>, pos: Pos) -> Result<Stmt, ParseErr> {
     match tokens.next() {
         Some(Token::Keyword(range, KeywordKind::PrintI32)) => {
-            let expr = expr(tokens)?;
+            let (_, expr) = expr(tokens, pos)?;
             Ok(Stmt::PrintI32(range, expr))
         }
         Some(Token::Keyword(range, KeywordKind::PrintStr)) => {
-            let expr = expr(tokens)?;
+            let (_, expr) = expr(tokens, pos)?;
             Ok(Stmt::PrintStr(range, expr))
         }
-        _ => Err(ParseErr::KeywordExpected),
+        Some(tok) => Err(ParseErr::KeywordExpected(tok.locate())),
+        _ => Err(ParseErr::KeywordExpected(pos.into())),
     }
 }
 
@@ -86,8 +91,9 @@ fn program(tokens: &mut PutBack<impl Iterator<Item = Token>>) -> Vec<Result<Stmt
         match tok {
             Token::Newline(_) => (),
             _ => {
+                let pos = tok.locate().end;
                 tokens.put_back(tok);
-                results.push(stmt(tokens));
+                results.push(stmt(tokens, pos));
             }
         }
     }

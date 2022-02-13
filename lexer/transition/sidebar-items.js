d6782df3dc0c6f037b76441234ecf298854df134
0918nobita <0918nobita@users.noreply.github.com>
2022-02-13 06:53:11 +0000

    deploy: e1fdb6015846431990ecb279ca02f78a54367dda

--- lexer/transition/sidebar-items.js
+++ lexer/transition/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["default","初期状態のとき、または他の状態で想定外の文字が出現したときに行う状態遷移"],["i32",""],["keyword",""],["str",""]]});
+initSidebarItems({"fn":[["transition",""]],"mod":[["i32",""],["initial","初期状態のとき、または他の状態で想定外の文字が出現したときに行う状態遷移"],["keyword",""],["str",""]]});

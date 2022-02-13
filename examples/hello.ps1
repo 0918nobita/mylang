33bb127b204f6019e13b0888492e39ed35f5205f
0918nobita <nobita.0918@gmail.com>
2022-02-13 17:39:48 +0900

    :hammer: mylang プログラムの Windows 向けビルド・起動用スクリプトを修正した

--- examples/hello.ps1
+++ examples/hello.ps1
@@ -1,4 +1,4 @@
-cargo run --bin lexer -- hello.mylang -o hello.tok.json
-cargo run --bin parser -- hello.tok.json -o hello.ast.json
-cargo run --bin ast_to_bytecode -- hello.ast.json -o hello.bytecode
-cargo run --bin bytecode_interp -- hello.bytecode
+cargo run --bin lexer -- hello.mylang -o hello.tok.json && `
+    cargo run --bin parser -- hello.tok.json -o hello.ast.json && `
+    cargo run --bin ast_to_bytecode -- hello.ast.json -o hello.bytecode && `
+    cargo run --bin bytecode_interp -- hello.bytecode

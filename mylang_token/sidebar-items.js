ece376828f58239da2f888563955b70802f7a5e0
0918nobita <0918nobita@users.noreply.github.com>
2022-02-14 17:38:37 +0000

    deploy: 49b9ccf0127dcac34bf8988e9bdcac46a6bfbcdf

--- mylang_token/sidebar-items.js
+++ mylang_token/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["KeywordKind",""],["Token",""]],"macro":[["pos",""],["range",""]],"mod":[["locatable",""],["pos",""],["range",""]],"struct":[["Pos","ソースコード上の位置"],["Range","ソースコード中の範囲"]],"trait":[["Locatable","ソースコード上で対応する範囲が特定できることを表現するトレイト"]]});
+initSidebarItems({"enum":[["KeywordKind",""],["Token",""]],"macro":[["pos","[`Pos`] を簡単に生成するためのマクロ"],["range","[`Range`] を簡単に生成するためのマクロ"]],"mod":[["locatable",""],["pos",""],["range",""]],"struct":[["Pos","ソースコード上の位置"],["Range","ソースコード中の範囲"]],"trait":[["Locatable","ソースコード上で対応する範囲が特定できることを表現するトレイト"]]});

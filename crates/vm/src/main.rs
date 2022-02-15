9983b323e1656d010d32a6e2754730a3cb532cc5
0918nobita <nobita.0918@gmail.com>
2022-02-15 19:14:31 +0900

    :sparkles: VM での入力バイトコードとして JSON とバイナリの両方が使えるようにした

--- crates/vm/src/main.rs
+++ crates/vm/src/main.rs
@@ -3,16 +3,28 @@ use std::{
     io::{self, BufRead, BufReader},
 };
 
-use clap::Parser;
+use clap::{Parser, ArgEnum};
 use mylang_bytecode::Inst;
 
 #[derive(Parser)]
 struct Opts {
     input: Option<String>,
+
+    #[clap(long = "input_format", arg_enum, default_value = "binary")]
+    input_format: InputFormat,
+}
+
+#[derive(Clone, ArgEnum)]
+enum InputFormat {
+    Json,
+    Binary,
 }
 
 fn main() -> anyhow::Result<()> {
-    let Opts { input } = Opts::parse();
+    let Opts {
+        input,
+        input_format,
+    } = Opts::parse();
 
     let stdin = io::stdin();
     let byte_code: Box<dyn BufRead> = if let Some(input) = input {
@@ -22,7 +34,10 @@ fn main() -> anyhow::Result<()> {
         Box::new(stdin.lock())
     };
 
-    let insts: Vec<Inst> = bincode::deserialize_from(byte_code)?;
+    let insts: Vec<Inst> = match input_format {
+        InputFormat::Json => serde_json::from_reader(byte_code)?,
+        InputFormat::Binary => bincode::deserialize_from(byte_code)?,
+    };
 
     mylang_vm::execute(&insts)?;
     Ok(())

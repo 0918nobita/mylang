0e013f0e4e3ad192f3c30a82be7ee09612f3061b
0918nobita <0918nobita@users.noreply.github.com>
2022-02-15 06:32:04 +0000

    deploy: e4f3674b91c90f042e46fcefac70bec72d61d331

--- mylang_lexer/state/sidebar-items.js
+++ mylang_lexer/state/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["State","字句解析器内部の状態"]],"mod":[["i32",""],["keyword",""],["str",""]]});
+initSidebarItems({"enum":[["State","字句解析器内部の状態"]],"mod":[["i32",""],["str",""],["symbol",""]]});

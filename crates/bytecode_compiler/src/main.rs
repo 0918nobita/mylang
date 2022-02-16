cd79fcd40ece8250d9373848a0212e016acd06ea
0918nobita <nobita.0918@gmail.com>
2022-02-16 16:16:07 +0900

    mylang_bytecode_compiler で stdin, stdout, input_format, output_format を指定できるようにした

--- crates/bytecode_compiler/src/main.rs
+++ crates/bytecode_compiler/src/main.rs
@@ -3,46 +3,93 @@ use std::{
     io::{self, BufRead, BufReader, BufWriter, Write},
 };
 
-use anyhow::Context;
-use clap::Parser;
+use anyhow::bail;
+use clap::{app_from_crate, Arg};
 use mylang_ast::Stmt;
+use mylang_cli_ext::{FileFormat, FILE_FORMAT_POSSIBLE_VALUES};
 
 use mylang_bytecode_compiler::ast_to_bytecode;
 
-#[derive(Parser)]
-struct Opts {
-    input: Option<String>,
-
-    #[clap(short = 'o')]
-    output: Option<String>,
-}
-
 fn main() -> anyhow::Result<()> {
-    let Opts { input, output } = Opts::parse();
+    let matches = app_from_crate!()
+        .arg(
+            Arg::new("input_format")
+                .long("input_format")
+                .visible_alias("if")
+                .possible_values((*FILE_FORMAT_POSSIBLE_VALUES).clone())
+                .default_value("json")
+                .help("Format of input AST"),
+        )
+        .arg(
+            Arg::new("stdin")
+                .long("stdin")
+                .takes_value(false)
+                .help("Read AST from stdin"),
+        )
+        .arg(
+            Arg::new("output_format")
+                .long("output_format")
+                .visible_alias("of")
+                .possible_values((*FILE_FORMAT_POSSIBLE_VALUES).clone())
+                .default_value("binary")
+                .help("Format of output bytecode"),
+        )
+        .arg(
+            Arg::new("stdout")
+                .long("stdout")
+                .takes_value(false)
+                .help("Write bytecode to stdout"),
+        )
+        .arg(Arg::new("input").required(false).help("Input AST file"))
+        .arg(
+            Arg::new("output")
+                .required(false)
+                .help("Output bytecode file"),
+        )
+        .get_matches();
+
+    let input_format = FileFormat::value_of(&matches, "input_format")?;
+    let use_stdin = matches.is_present("stdin");
+    let input = matches.value_of("input");
+
+    let output_format = FileFormat::value_of(&matches, "output_format")?;
+    let use_stdout = matches.is_present("stdout");
+    let output = matches.value_of("output");
 
     let stdin = io::stdin();
     let stdout = io::stdout();
 
-    let src: Box<dyn BufRead> = if let Some(input) = input {
-        let file = File::open(&input)?;
-        Box::new(BufReader::new(file))
-    } else {
-        Box::new(stdin.lock())
+    let src: Box<dyn BufRead> = match (use_stdin, input) {
+        (true, Some(_)) => bail!("Cannot specify both --stdin and [input]"),
+
+        (true, None) => Box::new(stdin.lock()),
+
+        (false, Some(path)) => Box::new(BufReader::new(File::open(path)?)),
+
+        (false, None) => bail!("No input specified. You can specify either --stdin or [input]"),
     };
 
-    let mut dest: Box<dyn Write> = if let Some(output) = output {
-        let file = File::create(output)?;
-        Box::new(BufWriter::new(file))
-    } else {
-        Box::new(BufWriter::new(stdout.lock()))
+    let mut dest: Box<dyn Write> = match (use_stdout, output) {
+        (true, Some(_)) => bail!("Cannot specify both --stdout and [output]"),
+
+        (true, None) => Box::new(stdout.lock()),
+
+        (false, Some(path)) => Box::new(BufWriter::new(File::create(path)?)),
+
+        (false, None) => bail!("No output specified. You can specify either --stdout or [output]"),
     };
 
-    let stmts: Vec<Stmt> = serde_json::from_reader(src)?;
+    let stmts: Vec<Stmt> = match input_format {
+        FileFormat::Json => serde_json::from_reader(src)?,
+        FileFormat::Binary => bincode::deserialize_from(src)?,
+    };
 
     let insts = ast_to_bytecode(&stmts);
 
-    let encoded = bincode::serialize(&insts)?;
+    match output_format {
+        FileFormat::Json => serde_json::to_writer_pretty(&mut dest, &insts)?,
+        FileFormat::Binary => bincode::serialize_into(&mut dest, &insts)?,
+    }
 
-    dest.write_all(&encoded)
-        .context("Failed to output bytecode")
+    Ok(())
 }
--- examples/hello.ps1
+++ examples/hello.ps1
@@ -1,5 +1,5 @@
 cargo build && `
     & ..\target\debug\mylang_lexer -o hello.tok.json hello.mylang && `
-    & ..\target\debug\mylang_parser -o hello.ast.json hello.tok.json && `
-    & ..\target\debug\mylang_bytecode_compiler -o hello.bytecode hello.ast.json && `
+    & ..\target\debug\mylang_parser hello.tok.json hello.ast.json && `
+    & ..\target\debug\mylang_bytecode_compiler hello.ast.json hello.bytecode && `
     & ..\target\debug\mylang_vm hello.bytecode

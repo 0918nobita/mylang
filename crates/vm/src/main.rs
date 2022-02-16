a85313931de1d1d85522421a5812437e5bde4e87
0918nobita <nobita.0918@gmail.com>
2022-02-16 12:25:52 +0900

    :sparkles: mylang_vm CLI ツールに stdin フラグを追加した

--- crates/vm/src/main.rs
+++ crates/vm/src/main.rs
@@ -3,24 +3,44 @@ use std::{
     io::{self, BufRead, BufReader},
 };
 
+use anyhow::bail;
 use clap::{app_from_crate, Arg};
 use mylang_bytecode::Inst;
 use mylang_cli_ext::{input_format_arg, value_of_input_format, InputFormat};
 
 fn main() -> anyhow::Result<()> {
     let matches = app_from_crate!()
-        .arg(Arg::new("input").required(false))
-        .arg(input_format_arg().default_value("binary"))
+        .arg(
+            input_format_arg()
+                .default_value("binary")
+                .help("Format of input bytecode"),
+        )
+        .arg(
+            Arg::new("stdin")
+                .long("stdin")
+                .takes_value(false)
+                .help("Read input bytecode from stdin"),
+        )
+        .arg(
+            Arg::new("input")
+                .required(false)
+                .help("Input bytecode file"),
+        )
         .get_matches();
+
     let input_format = value_of_input_format(&matches)?;
+    let use_stdin = matches.is_present("stdin");
     let input = matches.value_of("input");
 
     let stdin = io::stdin();
-    let byte_code: Box<dyn BufRead> = if let Some(input) = input {
-        let file = File::open(&input)?;
-        Box::new(BufReader::new(file))
-    } else {
-        Box::new(stdin.lock())
+    let byte_code: Box<dyn BufRead> = match (use_stdin, input) {
+        (true, Some(_)) => bail!("Cannot specify both --stdin and [input]"),
+
+        (true, None) => Box::new(stdin.lock()),
+
+        (false, Some(path)) => Box::new(BufReader::new(File::open(path)?)),
+
+        (false, None) => bail!("No input specified. You can specify either --stdin or [input]"),
     };
 
     let insts: Vec<Inst> = match input_format {

0d6449fccf02c7071bb779e5932fdd33d32f7a1d
0918nobita <0918nobita@users.noreply.github.com>
2022-10-10 08:33:54 +0000

    deploy: f9996a5c2c5da6ee217bb96d4be6b1b67ffd8569

---
+++ implementors/core/error/trait.Error.js
@@ -0,0 +1,6 @@
+(function() {var implementors = {
+"mylang_ast_interp":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"mylang_ast_interp/enum.AstInterpError.html\" title=\"enum mylang_ast_interp::AstInterpError\">AstInterpError</a>"]],
+"mylang_lexer":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"mylang_lexer/result/enum.LexErr.html\" title=\"enum mylang_lexer::result::LexErr\">LexErr</a>"]],
+"mylang_parser":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"mylang_parser/result/enum.ParseErr.html\" title=\"enum mylang_parser::result::ParseErr\">ParseErr</a>"]],
+"mylang_vm":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"mylang_vm/enum.VMError.html\" title=\"enum mylang_vm::VMError\">VMError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"mylang_token":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"mylang_token/pos/struct.Pos.html\" title=\"struct mylang_token::pos::Pos\">Pos</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"mylang_token/range/struct.Range.html\" title=\"struct mylang_token::range::Range\">Range</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

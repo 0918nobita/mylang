60dc0ef14181b71284260c465cdaf7b427cb5601
0918nobita <0918nobita@users.noreply.github.com>
2023-03-21 08:21:55 +0000

    deploy: f3931866b3bb8e75979606c640ae7c5a2afad6df

--- implementors/mylang_token/locatable/trait.Locatable.js
+++ implementors/mylang_token/locatable/trait.Locatable.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"mylang_ast":[["impl Locatable for <a class=\"enum\" href=\"mylang_ast/expr/enum.Expr.html\" title=\"enum mylang_ast::expr::Expr\">Expr</a>"],["impl Locatable for <a class=\"enum\" href=\"mylang_ast/stmt/enum.Stmt.html\" title=\"enum mylang_ast::stmt::Stmt\">Stmt</a>"]],
+"mylang_ast":[["impl Locatable for <a class=\"enum\" href=\"mylang_ast/stmt/enum.Stmt.html\" title=\"enum mylang_ast::stmt::Stmt\">Stmt</a>"],["impl Locatable for <a class=\"enum\" href=\"mylang_ast/expr/enum.Expr.html\" title=\"enum mylang_ast::expr::Expr\">Expr</a>"]],
 "mylang_lexer":[["impl Locatable for <a class=\"enum\" href=\"mylang_lexer/result/enum.LexErr.html\" title=\"enum mylang_lexer::result::LexErr\">LexErr</a>"]],
 "mylang_parser":[["impl Locatable for <a class=\"enum\" href=\"mylang_parser/result/enum.ParseErr.html\" title=\"enum mylang_parser::result::ParseErr\">ParseErr</a>"]],
 "mylang_token":[]

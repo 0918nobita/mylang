52b9e13df1f422a3c1b29d0d2a8a62a5f163b85c
Kodai Matsumoto <nobita.0918@gmail.com>
2023-08-02 17:25:05 +0900

    ソースファイル文字列をそのまま console.log する js を生成するようにした

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
 fn main() {
-    println!("mylang CLI")
+    println!("Compiling source.mylang ...");
+
+    let source = fs::read_to_string("source.mylang").unwrap();
+    let source = source.trim_end();
+    let source = source.replace('\n', "\\n");
+
+    let out = format!("'use strict';\nconsole.log('{}');\n", source);
+
+    println!("> out.js");
+    fs::write("out.js", out).unwrap();
 }

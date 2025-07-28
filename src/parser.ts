b7a120e5a931bc13ec9e67c415c6a112b66c044c
0918nobita <nobita.0918@gmail.com>
2025-07-28 20:54:02 +0900

    文字列を位置情報付きの文字の配列に変換するロジックを実装した

--- src/parser.ts
+++ src/parser.ts
@@ -1,7 +1,32 @@
 export const parse = (source: string) => {
     const segmenter = new Intl.Segmenter();
 
-    const segments = Array.from(segmenter.segment(source));
+    type Acc = {
+        currentPos: { line: number; column: number };
+        chars: Array<{ c: string; pos: { line: number; column: number } }>;
+    };
 
-    console.log(segments.map((segment) => segment.segment));
+    const { chars } = Array.from(segmenter.segment(source))
+        .map((segment) => segment.segment)
+        .reduce<Acc>(
+            ({ currentPos, chars }, item) => {
+                if (item === '\n') {
+                    return {
+                        currentPos: { line: currentPos.line + 1, column: 0 },
+                        chars: [...chars, { c: '\n', pos: currentPos }],
+                    };
+                }
+
+                return {
+                    currentPos: {
+                        line: currentPos.line,
+                        column: currentPos.column + 1,
+                    },
+                    chars: [...chars, { c: item, pos: currentPos }],
+                };
+            },
+            { currentPos: { line: 0, column: 0 }, chars: [] }
+        );
+
+    console.log(chars);
 };
